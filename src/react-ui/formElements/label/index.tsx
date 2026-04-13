4ad43e3e8527ad099477e735280ee2554df7dc19
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-04-12 20:54:59 -0600

    fix styling problem with Label

--- src/react-ui/formElements/label/index.tsx
+++ src/react-ui/formElements/label/index.tsx
@@ -6,7 +6,7 @@ type LabelProps = {
 
 const Label: FC<LabelProps> = ({ text }) => {
   return (
-    <label className='tw:block tw:text-[.9rem] tw:mb-[4px] tw:w-[100%]'>{text}</label>
+    <label className='bka-label'>{text}</label>
   );
 };
 
