a9f74e230ffab7b0ae41a86fe60e42f676fffab8
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-07-05 23:53:59 -0600

    add placeholder to textarea

--- src/react-ui/formElements/textarea/index.tsx
+++ src/react-ui/formElements/textarea/index.tsx
@@ -8,6 +8,7 @@ type TextareaProps = {
   full?: boolean | "true";
   rows?: number;
   autofocus?: boolean;
+  placeholder?: string;
 };
 
 const Textarea: FC<TextareaProps> = ({
@@ -17,10 +18,12 @@ const Textarea: FC<TextareaProps> = ({
   full = false,
   rows = 2,
   autofocus = false,
+  placeholder,
 }) => {
   const addedProps: Obj = {};
   if (autofocus) addedProps.autoFocus = autofocus;
   if (rows !== 0) addedProps.rows = rows;
+  if (placeholder) addedProps.placeholder = placeholder;
   return (
     <textarea
       className={`bka-form-element bka-textarea ${full ? "bka-form-element-full" : ""}`}
