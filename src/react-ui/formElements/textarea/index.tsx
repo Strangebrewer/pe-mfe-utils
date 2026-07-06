f14c9cd9ff7b183992e808d91a794faa37493330
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-07-05 19:49:51 -0600

    update textarea props

--- src/react-ui/formElements/textarea/index.tsx
+++ src/react-ui/formElements/textarea/index.tsx
@@ -6,6 +6,7 @@ type TextareaProps = {
   value: any;
   onChange: (e: any) => any;
   full?: boolean | "true";
+  rows?: number;
   autofocus?: boolean;
 };
 
@@ -14,10 +15,12 @@ const Textarea: FC<TextareaProps> = ({
   value,
   onChange,
   full = false,
+  rows = 2,
   autofocus = false,
 }) => {
   const addedProps: Obj = {};
   if (autofocus) addedProps.autoFocus = autofocus;
+  if (rows !== 0) addedProps.rows = rows;
   return (
     <textarea
       className={`bka-form-element bka-textarea ${full ? "bka-form-element-full" : ""}`}
