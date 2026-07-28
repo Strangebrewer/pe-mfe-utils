32529ec0d9c25a4d4776a304d8cf66557eabfe84
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-07-28 00:51:41 -0600

    add base gql api to utils

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./react-ui";
 export * from "./webpackConfigDefaults";
 export * from "./tracer-store";
 export { default as BaseRESTApi } from "./api/baseRESTApi";
+export { default as baseGqlRequest } from "./api/baseGqlRequest";
